b78b4bfd3190d830ed61af5770e3e88107304d72
Doston Shernazarov <98680065+dostonshernazarov@users.noreply.github.com>
2023-11-21 21:59:45 +0500

    config.py

--- data/config.py
+++ data/config.py
@@ -7,4 +7,3 @@ env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
